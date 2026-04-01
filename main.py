e0ed8508562b1ecce1c0ca8e7d027935743ba9fb
RZFeeser <rzfeeser@users.noreply.github.com>
2026-04-01 18:16:32 -0400

    Fix typo in print function call

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 def main():
     """runtime code"""
     print("Welcome to BatmanAPI")
-    prrint("this will fail there is no prrrrrnt")
+    print("this will fail there is no prrrrrnt")
 
 if __name__ == "__main__":
     main()
